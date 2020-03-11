e6403cfb0351b9aeb0595c1809165ba00cba8f00
flyq <flyq951@gmail.com>
2020-03-11 10:53:31 +0000

    finish the demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use ethkey::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let key = EthAccount::load_or_generate("./test.json", "test")
+        .expect("should load or generate new eth key");
+
+    println!("{:?}", key.address());
+
+    let message = [7_u8; 32];
+
+    // sign the message
+    let signature = key.sign(&message).unwrap();
+
+    // verify the signature
+    let result = key.verify(&signature, &message).unwrap();
+    println!("{}", if result {"verification ok"} else {"wrong signature"});
+    key.change_password("test");
+    println!("{:?}", key);
+    println!("{:?}", key.kestore_path());
+    println!("{:?}", key.public());
 }
